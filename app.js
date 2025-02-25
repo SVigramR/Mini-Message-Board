352637648d03a01bed89c6801ff2dfdc11447015
SVigramR <87016515+SVigramR@users.noreply.github.com>
2025-02-25 23:18:01 +0530

    Added index view template with sample data

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
 const newRouter = require("./routes/newRouter");
 const indexRouter = require("./routes/indexRouter");
 
 const PORT = process.env.PORT || 3000;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use("/new", newRouter)
 app.use("/", indexRouter)
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,8 +2,22 @@ const { Router } = require("express")
 
 const indexRouter = Router();
 
+const messages = [
+    {
+      text: "Amazing!",
+      user: "Jake",
+      added: new Date()
+    },
+    {
+      text: "Howdy!",
+      user: "Marco",
+      added: new Date()
+    }
+  ];
+  
+
 indexRouter.get("/", (req, res) => {
-    res.send("Hello, world!")
+  res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
 
