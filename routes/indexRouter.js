6d3bc5b31fc0ebfeeabb7f552bab5e7b07c38228
SVigramR <87016515+SVigramR@users.noreply.github.com>
2025-02-26 14:09:58 +0530

    Added view template (form) for new route and appending the form data to messages array with redirect

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -20,5 +20,12 @@ indexRouter.get("/", (req, res) => {
   res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
+indexRouter.post("/new",(req, res) => {
+  const messageText = req.body.messageText;
+  const messageUser = req.body.messageUser;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect("/")
+})
+
 
 module.exports = indexRouter;
--- routes/newRouter.js
+++ routes/newRouter.js
@@ -2,6 +2,9 @@ const { Router } = require("express")
 
 const newRouter = Router();
 
-newRouter.get("/",(req, res) => res.send("New Page"));
+newRouter.get("/",(req, res) => {
+    res.render("form")
+    
+});
 
 module.exports = newRouter;
